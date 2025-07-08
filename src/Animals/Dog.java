dcae6a726680507e4674a69dc84088bbebb6a3f0
csbsantos <claudiasbsantos13@gmail.com>
2025-07-08 11:04:23 +0100

    Update Dog.java

--- src/Animals/Dog.java
+++ src/Animals/Dog.java
@@ -1,4 +1,18 @@
 package Animals;
 
-public class Dog {
+public class Dog extends Animals{
+    private String breed;
+
+    public Dog(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+    @Override
+    public  void makeNoise (){
+        System.out.println("Woof woof woof");
+    }
+
+    public void runAfterMailman (){
+        System.out.println("Run, mailman. I'm chasing you. Woof");
+    }
 }
