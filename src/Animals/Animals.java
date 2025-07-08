a00f481b607fab6d674bbde2c439c646ff30fd54
csbsantos <claudiasbsantos13@gmail.com>
2025-07-08 11:04:42 +0100

    Update Animals.java

--- src/Animals/Animals.java
+++ src/Animals/Animals.java
@@ -1,4 +1,28 @@
 package Animals;
 
 public class Animals {
+    private String name;
+    private int hungerLevel;
+    private int age;
+
+    public Animals(String name) {
+        this.name = name;
+        this.hungerLevel=20;
+        this.age=0;
+
+    }
+    public void eat (String food){
+        if (hungerLevel<10){
+            System.out.println("I'm not hungry, thanks");
+            return;
+        }
+        System.out.println("Huuum. This " + food + " is delicious!");
+    }
+
+    public void getOlder (){
+        age++;
+    }
+    public void makeNoise (){
+
+    }
 }
