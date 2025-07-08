ce789701d9adc89847ae74bb04f868ff8f6088a4
csbsantos <claudiasbsantos13@gmail.com>
2025-07-08 11:04:59 +0100

    Update AnimalsZoo.java

--- src/Animals/AnimalsZoo.java
+++ src/Animals/AnimalsZoo.java
@@ -1,4 +1,22 @@
 package Animals;
 
 public class AnimalsZoo {
+    public static void main(String[] args) {
+        Pig joe = new Pig("Joseph");
+        Dog charles = new Dog("Charlie", "Golden");
+        Bird pet = new Bird("Peter");
+
+        pet.eat("leaves");
+        charles.runAfterMailman();
+        pet.makeNoise();
+        charles.makeNoise();
+        joe.makeNoise();
+
+
+        Animals johny = new Dog("Johny", "Labrador");
+        johny.makeNoise();
+        if (johny instanceof Dog d) {
+            d.runAfterMailman();
+        }
+    }
 }
