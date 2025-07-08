901a6a972b6e9d796bdc276d21f148918f5b5869
csbsantos <claudiasbsantos13@gmail.com>
2025-07-08 11:03:46 +0100

    Update Bird.java

--- src/Animals/Bird.java
+++ src/Animals/Bird.java
@@ -1,4 +1,13 @@
 package Animals;
 
-public class Bird {
+public class Bird extends Animals {
+
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeNoise (){
+        System.out.println("piu piu piu");
+    }
 }
